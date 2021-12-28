23f31c76d1532db421a8713170760dca52f6f29e
Jitendra Mahatpure <jmahatpure01@gmail.com>
2021-12-28 15:22:48 +0530

    fixing asset path

---
+++ _next/static/W-HvrQnVsNhQYYCAT1M93/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7f63bc07b1423c62.css","static/chunks/pages/index-02392231096e043d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
