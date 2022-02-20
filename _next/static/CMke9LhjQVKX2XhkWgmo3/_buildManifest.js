37d5f59de31194cc56153bdd9c7a3150d061aacb
Jitendra Mahatpure <jmahatpure01@gmail.com>
2022-02-20 19:42:01 +0530

    font change

--- _next/static/CMke9LhjQVKX2XhkWgmo3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/125-cec551a2cc4738d2.js","static/css/b74f2be21a3cf689.css","static/chunks/pages/index-ba22662b72fbdb75.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
