766e4576b932eeb86d0206d8564509fa24a5c0ea
Jitendra Mahatpure <jmahatpure01@gmail.com>
2023-07-15 19:45:22 +0530

    resume update, about section

---
+++ _next/static/seSQ9PfEViCntljcszdZc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b991c0285af020a7.css","static/chunks/pages/index-d0b47e37f9cd5d2e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
