00ec4693780fb6a13673f9fb0483d4239b7ea1bc
Jitendra Mahatpure <jmahatpure01@gmail.com>
2024-12-23 10:15:57 +0530

    updated resume

---
+++ _next/static/q54GZGk47udNpElenhtdT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b991c0285af020a7.css","static/chunks/pages/index-06692b90be2d5e5c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
